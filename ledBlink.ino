c22cce1d7a46c42745845a14efe68ffc58b9a256
zacharywilsonnyu <zw1582@nyu.edu>
2021-04-16 04:06:26 -0400

    added a second led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,13 +1,17 @@
 int ledPin = 13;
+int ledPin2 = 14;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
     digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
     delay(500);
     digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
     delay(500);
 
 }
