ce299e471689cbfe4924519f9a64172489dd6224
zacharywilsonnyu <zw1582@nyu.edu>
2021-04-16 04:00:13 -0400

    added led blink code

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
 
 }
