c94d09558b68d1daaa225b46a4fab71331ee341f
zacharywilsonnyu <zw1582@nyu.edu>
2021-04-16 04:11:11 -0400

    added a button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,17 +1,20 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int buttonPin = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+  if (digitalRead(buttonPin) == HIGH) {
     digitalWrite(ledPin, HIGH);
     digitalWrite(ledPin2, HIGH);
     delay(500);
     digitalWrite(ledPin, LOW);
     digitalWrite(ledPin2, LOW);
     delay(500);
-
+  }
 }
